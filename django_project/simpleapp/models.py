7dc8bc45b03d8c1cc49c1ed83d54dbf3ca41e3d2
En5LaVeR <kavusspk@mail.ru>
2024-06-07 23:33:11 +1000

    add filter

--- django_project/simpleapp/models.py
+++ django_project/simpleapp/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.urls import reverse
 
 
-# Товар для нашей витрины
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
     category = models.ForeignKey(to='Category', on_delete=models.CASCADE, related_name='products')
     price = models.FloatField(validators=[MinValueValidator(0.0)])
+    material = models.ManyToManyField(Material, through='ProductMaterial')
 
     def __str__(self):
         return f'{self.name.title()}: {self.description}, {self.price} р'
+
+
+class ProductMaterial(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
 
 
 class Category(models.Model):
